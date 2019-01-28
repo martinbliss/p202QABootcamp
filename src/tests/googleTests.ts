60a2079274bb199759a9483b97fdfbbcf2febb83
Martin Bliss <martin.bliss@projekt202.com>
2019-01-28 15:32:26 -0600

    Add shortcut test run command

--- src/tests/googleTests.ts
+++ src/tests/googleTests.ts
@@ -3,25 +3,25 @@ import { SearchBar, SearchSuggestion, SearchResult } from '../api/google';
 
 fixture('Getting Started').page('http://www.google.com');
 
-test('Can search for cats and see that a search suggestion "cats" is present', async test => {
+test('Can search for cats and see that a search suggestion "cats" is present', async (test) => {
     await test.typeText(SearchBar, 'cats')
         .expect(SearchSuggestion('cats').textContent).eql('cats');
 });
 
-test('Can see the first result of searching for cats is a complete guide to caring for cats', async test => {
+test('Can see the first result of searching for cats is a complete guide to caring for cats', async (test) => {
     await test.typeText(SearchBar, 'cats')
         .click(SearchSuggestion('cats'))
-        .wait(3000)
+        // .wait(3000)
         .expect(SearchResult('Complete Guide to Caring for Cats | Cat Breed Information, Cat ...').exists).eql(true);
 });
 
-test('Can search for dogs', async test => {
+test('Can search for dogs', async (test) => {
     await test.typeText(SearchBar, 'dogs')
         .pressKey('enter')
         .expect(SearchResult('Dog - Wikipedia').exists).eql(true);
 });
 
-test('Can see the first result of searching for dogs is "Complete Guide to Caring for Dogs | Dog Breed Information, Dog ..."', async test => {
+test('Can see the first result of searching for dogs is "Complete Guide to Caring for Dogs | Dog Breed Information, Dog ..."', async (test) => {
     await test.typeText(SearchBar, 'dogs')
         .pressKey('enter')
         .expect(SearchResult('Complete Guide to Caring for Dogs | Dog Breed Information, Dog ...').exists).eql(true);
